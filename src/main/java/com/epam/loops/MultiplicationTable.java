51a00a5a24c04ca026558b79ff01bd44b1e939e0
emmanuel hernandez <emmanuelhern00@gmail.com>
2022-12-25 22:11:05 -0400

    resolving multiplication test

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -5,14 +5,11 @@ import java.util.Scanner;
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
-    	
-    	try(Scanner sc = new Scanner(System.in)){
-    		int inputToMultiply = sc.nextInt();
-    		
-    		for(int multiplier = 1; multiplier <=10; multiplier++) {
-    			System.out.println(multiplier * inputToMultiply);
-    		}
-    	}
+
+        for (int multiplier = 1; multiplier <= 10; multiplier++) {
+            System.out.println(multiplier + " x " +  numberTableToPrint + " = " + (multiplier * numberTableToPrint));
+        }
     }
+
 
 }
